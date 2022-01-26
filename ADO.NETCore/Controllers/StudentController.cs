72ddfa2718d3acc5f10519bb12495a4add512b86
athifhere <athifhere@gmail.com>
2022-01-26 12:47:21 +0530

    Add project files.

---
+++ ADO.NETCore/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ADO.NETCore.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
