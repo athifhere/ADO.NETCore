ac794386fb26afdf40c6eeacf864e2343a6b890e
athifhere <athifhere@gmail.com>
2022-01-27 11:45:23 +0530

    Dapper

--- ADO.NETCore/DataAccess/DAL.cs
+++ ADO.NETCore/DataAccess/DAL.cs
@@ -1,107 +1,62 @@
 ﻿using ADO.NETCore.Models;
 using System.Data.SqlClient;
 using System.Data;
+using Dapper;
 
 namespace ADO.NETCore.DataAccess
 {
     public class DAL
     {
         string connectionString = "data source=DESKTOP-7OCUR3A; initial catalog =Assignment;Integrated Security=True";
+
         public IEnumerable<StudentModel> GetAllStudents()
         {
             List<StudentModel> lstStudents = new List<StudentModel>();
-            using(SqlConnection conn = new SqlConnection(connectionString))
+            using (IDbConnection db = new SqlConnection(connectionString))
             {
-                SqlCommand cmd = new SqlCommand("select * from Student", conn);
-                conn.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    StudentModel student = new StudentModel();
-                    student.Id = Convert.ToInt32(rdr["Id"]);
-                    student.FirstName = rdr["FirstName"].ToString();
-                    student.LastName = rdr["LastName"].ToString();
-                    student.Email = rdr["Email"].ToString();
-                    student.Mobile = rdr["Mobile"].ToString();
-                    student.Address = rdr["Address"].ToString();
-
-                    lstStudents.Add(student);
-                }
-                conn.Close();
+                lstStudents = db.Query<StudentModel>("Select * From Student").ToList();
             }
             return lstStudents;
         }
 
         public void CreateStudent(StudentModel model)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (IDbConnection db = new SqlConnection(connectionString))
             {
-                SqlCommand cmd = new SqlCommand
-                    ("insert into Student VALUES (@FirstName, @LastName, @Email, @Mobile, @Address)", conn);
-                cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
-                cmd.Parameters.AddWithValue("@LastName", model.LastName);
-                cmd.Parameters.AddWithValue("@Email", model.Email);
-                cmd.Parameters.AddWithValue("@Mobile", model.Mobile);
-                cmd.Parameters.AddWithValue("@Address", model.Address);
-                conn.Open();
-                cmd.ExecuteNonQuery();
+                string sqlQuery = "Insert Into Student Values(@FirstName, @LastName, @Email, @Mobile, @Address)";
+
+                int rowsAffected = db.Execute(sqlQuery, model);
             }
         }
 
         public void UpdateStudent(StudentModel model)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            string spName = "spUpdateStudent";
+            var parameters = new { @Id = model.Id, @FirstName = model.FirstName, @LastName = model.LastName, 
+                @Email = model.Email, @Mobile = model.Mobile, @Address = model.Address };
+
+            using(IDbConnection db = new SqlConnection(connectionString))
             {
-                string updateCommand = "UPDATE Student SET FirstName = @FirstName, " +
-                    "LastName = @LastName, Email = @Email, Mobile = @Mobile, Address = @Address " +
-                    "where Id = @Id";
-                SqlCommand cmd = new SqlCommand(updateCommand, conn);
-                cmd.Parameters.AddWithValue("@Id", model.Id);
-                cmd.Parameters.AddWithValue("@FirstName", model.FirstName);
-                cmd.Parameters.AddWithValue("@LastName", model.LastName);
-                cmd.Parameters.AddWithValue("@Email", model.Email);
-                cmd.Parameters.AddWithValue("@Mobile", model.Mobile);
-                cmd.Parameters.AddWithValue("@Address", model.Address);
-                conn.Open();
-                cmd.ExecuteNonQuery();
+                var affectedRows = db.Execute(spName, parameters, commandType: CommandType.StoredProcedure);
             }
         }
 
         public StudentModel GetStudent(int id)
         {
             StudentModel student = new StudentModel();
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (IDbConnection db = new SqlConnection(connectionString))
             {
-                string selectCommand = "select * from Student where Id = " + id;
-                SqlCommand cmd = new SqlCommand(selectCommand, conn);
-                conn.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    student.Id = Convert.ToInt32(rdr["Id"]);
-                    student.FirstName = rdr["FirstName"].ToString();
-                    student.LastName = rdr["LastName"].ToString();
-                    student.Email = rdr["Email"].ToString();
-                    student.Mobile = rdr["Mobile"].ToString();
-                    student.Address = rdr["Address"].ToString();
-                }
+                student = db.Query<StudentModel>("Select * From Student WHERE Id =" + id).SingleOrDefault();
             }
             return student;
         }
 
         public void DeleteStudent(int id)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (IDbConnection db = new SqlConnection(connectionString))
             {
-                //string deleteCommand = "delete from Student where Id = " + id;
-                string deleteCommand = "spDeleteStudent";
-                SqlCommand cmd = new SqlCommand(deleteCommand, conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@Id", id);
-                conn.Open();
-                cmd.ExecuteNonQuery();
+                string sqlQuery = "Delete from Student where Id=" + id;
+                int rowsAffected = db.Execute(sqlQuery);
             }
         }
 
